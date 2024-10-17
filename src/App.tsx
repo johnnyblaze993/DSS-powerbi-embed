8075641d15a7cd8d17d711985ac957d8b2a39842
Jonathan Alvarez <jalvarez@axientcorp.com>
2024-10-17 15:07:35 +0000

    Refactor App component and embed Power BI iframe

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,25 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+
+import './App.css';
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+
+
+      {/* Embed Power BI iframe */}
+      <div className="powerbi-container">
+        <iframe
+          title="RIMFIRE Reliability Analytics v2.0"
+          width="1140"
+          height="541.25"
+          src="https://app.high.powerbigov.us/reportEmbed?reportId=c300d03b-ef88-4b58-8ec5-a25734b8fc9b&autoAuth=true&ctid=3ffbc5ec-b740-451c-ab2d-36a9f55bfa3c"
+          frameBorder="0"
+          allowFullScreen={true}
+        ></iframe>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
