f14bfbb2ec6298c102683f3b5ad8226a8b79db4f
Jonathan Alvarez <jalvarez@axientcorp.com>
2024-10-18 16:03:24 +0000

    Configure Vite server to proxy requests to the backend

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,11 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/PBI/',
   plugins: [react()],
-})
+  server: {
+    proxy: {
+      '/PBI': 'http://localhost:18080', // Proxy requests to backend
+    },
+} }
+)
