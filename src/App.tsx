fcaa9b5e5dc1127b0d9aa89736ed74a8dd7459fe
Jonathan Alvarez <jalvarez@axientcorp.com>
2024-10-30 15:13:50 +0000

    Full screen power Bi app

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,26 @@
-
 import './App.css';
 
 function App() {
-
   return (
-    <>
 
+      <div style={{
+        width: '100vw',
+        height: '100vh',
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center'
 
-      {/* Embed Power BI iframe */}
-      <div className="powerbi-container">
+      }}>
+        {/* <h1>RIMFIRE Reliability Analytics v2.0</h1> */}
         <iframe
           title="RIMFIRE Reliability Analytics v2.0"
-          width="1140"
-          height="541.25"
+          style={{ width: '100%', height: '100%', border: 'none' }}
           src="https://app.high.powerbigov.us/reportEmbed?reportId=c300d03b-ef88-4b58-8ec5-a25734b8fc9b&autoAuth=true&ctid=3ffbc5ec-b740-451c-ab2d-36a9f55bfa3c"
           frameBorder="0"
           allowFullScreen={true}
         ></iframe>
       </div>
-    </>
+  
   );
 }
 
